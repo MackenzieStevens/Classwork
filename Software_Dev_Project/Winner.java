54958c7555a10cd8da8fc6f73ff031c6dcfe4d80
MackenzieStevens <98902702+MackenzieStevens@users.noreply.github.com>
2022-04-07 12:48:02 -0400

    Update Winner.java

--- Software_Dev_Project/Winner.java
+++ Software_Dev_Project/Winner.java
@@ -3,8 +3,8 @@ package Software_Dev_Project;
 import java.util.ArrayList;
 
 public class Winner {
-    public void decideWinner(ArrayList<hand> hand, Object player) {
-        if (hand.length != 0) {
+    public void decideWinner(ArrayList<Hand> hand, Object player) {
+        if (hand.size() != 0) {
             return;
         }
         else {
