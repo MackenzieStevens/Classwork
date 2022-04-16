794ae1d3c3c41c62264f1b93306373cbfa525ad0
MackenzieStevens <98902702+MackenzieStevens@users.noreply.github.com>
2022-04-15 22:51:45 -0400

    start of turn system

--- checkCard/GameB.java
+++ checkCard/GameB.java
@@ -1,4 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GameB {
+    static String guess;
     public static void main(String[] args) {
     DeckB deck = new DeckB();
     HandB hand1 = new HandB();
@@ -6,7 +10,35 @@ public class GameB {
     player1.showPlayerHand();
     player1.checkForBooks();
     player1.showPlayerHand();
+    Scanner input = new Scanner(System.in);
+    do{
+    System.out.println("Player 1's turn");
+    String guess = input.nextLine();
+    }while();
     }
     
+    public static void match1(){
+        HandB hand1 =new HandB();
+        for(int i = 0; i<hand1.getHandLength(); i++){
+            if(hand1.getHandB().equals(guess)){
+                System.out.println("Match!");
+                hand1.addToHand(i);
+                player2.remove(i);
+
+            }
+            else{
+                System.out.println("Go fish.");
+                CardB playingCard = new CardB();
+                Random rand = new Random();
+                DeckB playDeck =new DeckB();
+                int num = playDeck.getSize();
+                int randNum = rand.nextInt(num);
+                playingCard = playDeck.getCard(randNum);
+                hand1.addToHand(playingCard);
+                num--;
+            }
+            
+        }
+    }
     
 }
